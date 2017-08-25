d863e052e05361731d39213565405f44223a69c0
Inga Kuznecova <i.kuznetsova@dunice.net>
2017-08-25 16:23:40 +0300

    changed scoreapiclient.js: added in function to return response

--- apiclient/scoresapiclient.js
+++ apiclient/scoresapiclient.js
@@ -7,10 +7,14 @@ function subject(data, APIKey, baseURL) {
     var endpoint = 'subjects';
     if(!auth.ScoresAPI) {
         return login('ScoresAPI', APIKey, baseURL).then(function () {
-            return  request(baseURL+'subject.json?auth_type=1', auth.ScoresAPI, data, endpoint);
+            return  request(baseURL+'subject.json?auth_type=1', auth.ScoresAPI, data, endpoint).then(function (response) {
+                return response;
+            });
         });
     } else {
-        return  request(baseURL+'subject.json?auth_type=1', auth.ScoresAPI, data, endpoint);
+        return  request(baseURL+'subject.json?auth_type=1', auth.ScoresAPI, data, endpoint).then(function (response) {
+            return response;
+        });
     }
 }
 
@@ -18,10 +22,14 @@ function dataQuery(data, APIKey, baseURL) {
     var endpoint = 'dateQuery';
     if(!auth.ScoresAPI) {
         return login('ScoresAPI', APIKey, baseURL).then(function () {
-            return request(baseURL+'dateQuery.json?auth_type=1', auth.ScoresAPI, data, endpoint);
+            return request(baseURL+'dateQuery.json?auth_type=1', auth.ScoresAPI, data, endpoint).then(function (response) {
+                return response;
+            });
         });
     } else {
-        return request(baseURL+'dateQuery.json?auth_type=1', auth.ScoresAPI, data, endpoint);
+        return request(baseURL+'dateQuery.json?auth_type=1', auth.ScoresAPI, data, endpoint).then(function (response) {
+            return response;
+        });
     }
 }
 
