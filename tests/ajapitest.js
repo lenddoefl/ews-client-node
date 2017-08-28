f481a1776e777c285beaa05a007ad00983208422
Inga Kuznecova <i.kuznetsova@dunice.net>
2017-08-28 18:00:35 +0300

    added unit test for call getApplication, createAttachment, finishStep, finishSession

--- tests/ajapitest.js
+++ tests/ajapitest.js
@@ -4,23 +4,17 @@ var expect = chai.expect;
 
 describe('Applicant Journey CLient', function () {
     this.timeout(5000);
-    var data;
-    var uid;
-    before(function () {
+    var uid,
         data = ajModule.init({
             URL_AJ: process.argv[5],
             urlFolder: process.argv[6]
         });
-    });
+
     describe('Call login', function () {
-        it('Should come success response', function (done) {
+        it('Should come success response with status code 200, status message OK', function (done) {
             ajModule.login('AJAPI', data.APIKey, data.URL_AJ).then(function (response) {
                 expect(response.statusCode).to.equal(200);
                 expect(response.statusMessage).to.equal('OK');
-                expect(response.data.authToken).not.to.be.null;
-                expect(response.data.reqToken).not.to.be.null;
-                expect(response.data.authToken).to.not.be.empty;
-                expect(response.data.reqToken).to.not.be.empty;
 
                 done();
             });
@@ -28,24 +22,162 @@ describe('Applicant Journey CLient', function () {
     });
 
     describe('Call startSession', function () {
-        var dataForCall = {
-            applicant: {},
-            application: "sdkExample"
-        };
-        it('Should come success response', function (done) {
-            ajModule.startSession(dataForCall, data.APIKey, data.URL_AJ).then(function (response) {
+        it('Should come success response with status code 200, status message OK', function (done) {
+            var dataForCallStartSession = {
+                applicant: {},
+                application: "sdkExample"
+            };
+            ajModule.startSession(dataForCallStartSession, data.APIKey, data.URL_AJ).then(function (response) {
+                uid = response.data.uid;
+
                 expect(response.statusCode).to.equal(200);
                 expect(response.statusMessage).to.equal('OK');
-                expect(response.data.applicationHash).not.to.be.null;
-                expect(response.data.publicKey).not.to.be.null;
-                expect(response.data.uid).not.to.be.null;
-                expect(response.data.applicationHash).to.not.be.empty;
-                expect(response.data.publicKey).to.not.be.empty;
-                expect(response.data.uid).to.not.be.empty;
+
+                done();
+            });
+        });
+    });
+
+    describe('Call finishSession', function () {
+        it('Should come success response with status code 200, status message OK', function (done) {
+            var dataForCall = {
+                sequence: 0,
+                uid: uid
+            };
+
+            ajModule.finishSession(dataForCall, data.APIKey, data.URL_AJ).then(function (response) {
+                expect(response.statusCode).to.equal(200);
+                expect(response.statusMessage).to.equal('OK');
+
+                done();
+            });
+        });
+    });
+
+    describe('Call finishStep', function () {
+        it('Should come success response with status code 200, status message OK', function (done) {
+            var dataForCall = {
+                applicant: {},
+                device: {
+                    browser: null,
+                    deviceId: null,
+                    ipAddress: null,
+                    os: {
+                        type: null,
+                        version: null
+                    },
+                    referrer: null,
+                    viewport: {
+                        height: null,
+                        width: null
+                    }
+                },
+                metas: {},
+                observations: {},
+                sequence: 0,
+                state: {},
+                step: 'abGlobal',
+                uid: uid
+            };
+
+            ajModule.finishStep(dataForCall, data.APIKey, data.URL_AJ).then(function (response) {
+                expect(response.statusCode).to.equal(200);
+                expect(response.statusMessage).to.equal('OK');
+
+                done();
+            });
+        });
+    });
+
+    describe('Call createAttachment', function () {
+        it('Should come success response with status code 200, status message OK', function (done) {
+            var dataForCall = {
+                attachmentType: 'photo',
+                attachmentTypeVersion: '1.0',
+                contentType: 'image/jpeg',
+                inlineData: `/9j/4AAQSkZJRgABAQEASABIAAD/2wCEAAICAgICAQICAgIDAgIDAwYEAwMDAwcFBQQGCAcJCAgH
+                            CAgJCg0LCQoMCggICw8LDA0ODg8OCQsQERAOEQ0ODg4BAgMDAwMDBwQEBw4JCAkODg4ODg4ODg4O
+                            Dg4ODg4ODg4ODg4ODg4ODg4ODg4ODg4ODg4ODg4ODg4ODg4ODg4ODv/CABEIAMgAyAMBIgACEQED
+                            EQH/xAAYAAEBAQEBAAAAAAAAAAAAAAAACAkBB//aAAgBAQAAAADfwAAABCc+AAAHqejjEqBAAACw
+                            t7mJUCAAAFhb3MSoEAAALC3uYlQIAAAWFvcxKgQAAAsLe5iVAgAABYW9zEqBAAACwt7mJUCAAAFh
+                            b3MSoEAAALC3uYlQIAAAWFvcxKgQAAAsLe5iVAgAABYW9zEqBAAACwt7mJUCAAAFhb3MSoEAAALC
+                            3uYlQIAAAWFvcxKgQAAAsLe5iVAgAABYW9zEqBAAACwt7mJUCAAAFhb3MSoEAAALC3uZgycAAHeF
+                            G6xAAAAP/8QAGQEBAQEBAQEAAAAAAAAAAAAAAAcFBgME/9oACAECEAAAAPjxQB69DyMdAGre+Rjo
+                            A1b3yMdAGre+RjoA1b3yMdAGre+RjoA1b3yMdAGre+RjoA1b3yMdAGre+RjoA1b3zErAGlaQAH//
+                            xAAaAQEBAQEAAwAAAAAAAAAAAAAABwUEAQIG/9oACAEDEAAAAOfOAHvrYE+APPdUcCfADvqOBPgB
+                            31HAnwA76jgT4Ad9RwJ8AO+o4E+AHfUcCfADvqOBPgB31HAnwA76ji/EADsooAD/xAA1EAAAAQYO
+                            AgIBAwUAAAAAAAAEAAEDBQhWAgYJEBgZODlAdXaUlrPR0wcRIRITkRUxMlFh/9oACAEBAAE/AMU0
+                            g1os/hD56RRRDqZANQQ1OHHGTQ0J4cL7SJEsE5v8zf2/bN/JVjK4dwNtD+wqxlcu4H2h/aVYyuXc
+                            DbQ/sKsZXLuBtof2FWMrl3A+0P7SrGVy7gfaH9pVjK5dwNtD+wqxlcu4G2h/YVYyuXcD7Q/tKsZX
+                            LuBtof2FWMrl3A20P7CrGVy7gbaH9hVjK5dwNtD+wqxlcu4G2h/YVYyuXcDbQ/sKsZXLuBtof2lW
+                            Mrl3Q2zP7SrGVy7obZn9pVjK4dwPtD+0qxlcu4H2h/aVYyuXcDbQ/sL4GbKjV8xtmRbiD/RleBUA
+                            pWDRIxKcNCgpzw0SM0KAaAf9w5jG/wB/Zj/f/J5RC24C0oC7xOGYOvL4rZKsuk08ohbcBaUBd4nD
+                            MHXl8VslWXSaeUQtuAtKAu8ThmDry+K2SrLpNPKIW3AWlAXeJwzB15fFbJVl0mnlELbgLSgLvE4Z
+                            g68vitkqy6TTyiFtwFpQF3icMwdeXxWyVZdJp5RC24C0oC7xOGYOvL4rZKsuk08ohbcBaUBd4nDM
+                            HXl8VslWXSaeUQtuAtKAu8ThmDry+K2SrLpNPKIW3AWlAXeJwzB15fFbJVl0mnlELbgLSgLvE4Zg
+                            68vitkqy6TTyiFtwFpQF3icMwdeXxWyVZdJp5RC24C0oC7xOGYOvL4rZKsuk08ohbcBaUBd4nDMH
+                            Xl8VslWXSaeUQtuAtKAu8ThmDry+K2SrLpNPKIW3AWlAXeJwzB15fFbJVl0mnlELbgLSgLvE4Zg6
+                            8vitkqy6TTyiFtwFpQF3icMwdeXxWyVZdJp5RC24C0oC7xOGYOvL4rZKsuk08ohbcBaUBd4nDMHX
+                            l8VslWXSaeUQtuAtKAu8ThmDry+K2SrLpNO2IzP8yfL7UwOM0QlIrViooEXwwRIlFrdGGhmTQEqa
+                            FCN+mF+fr6SQfyVA9ph1lLyVD4Kge0w6yl5Kh8FQPaYdZS8lQ+CoHtMOspeSIfBUD2mHWUvJUPgq
+                            B7TDrKXkqHwVA9ph1lLyRD4Kge0w6yl5Kh8FQPaYdZS8lQ+CoHtMOspeSofBUD2mHWUvJUPgqB7T
+                            DrKXkqHwVA9ph1lLyVD4Kge0w6yl5Kh8FQPaYdZS8kQ+CoHtMOspeSofBUD2mHWUvJEPgqBzS7rK
+                            XkiHwVA9ph1lLyVD4Kge0w6yl5Kh8FQPaYdZS8lQ+CZSZS+bPittdRx1jqolYAi+FVg1AlShl0iE
+                            QzQ0qI0GAb9EH8n/AD/GL//EACsRAAACBgoCAwEAAAAAAAAAAAEFAAIDEBU1BjBSU3FykaGxwQQR
+                            EiFRE//aAAgBAgEBPwDyfKY+Gy/o1H0CR8st7JHyy3skfLLeyR8st7JHyy3skfLLeyR8st7JHyy3
+                            skfLLeyR8st7IzOy9q0VZqLexWEAD6/XUklo4h3VlkxY5g5dSSWjiHdWWTFjmDl1JJaOId1ZZMWO
+                            YOXUklo4h3VlkxY5g5dSSWjiHdWWTFjmDl1JJaOId1ZZMWOYOXUklo4h3VlkxY5g5dSSWjiHdWWT
+                            FjmDl1JJaOId1ZZMWOYOXUklo4h3VlkxY5g5cfMWrfwPgyVFYfYfQfaQwxuVtBSGGNytoKQwxuVt
+                            BSGGNytoKQwxuVtBSGGNytoKQwxuVtBSGGNytoKQwxuVtBSGGNytoKF5f5zPzmSyzJYABYPY+h/a
+                            /wD/xAApEQABAQQLAAMBAQAAAAAAAAABBAADBTQCEBQVMFJTcYGRsRESITFB/9oACAEDAQE/AHz9
+                            2nofd4fgNeiLM16IszXoizNeiLM16IszXoizNeiLM16IszXoizNeiPM1CJJHlMUKJ/T+VRiT5GGi
+                            m3e49qjEnyMNFNu9x7VGJPkYaKbd7j2qMSfIw0U273HtUYk+Rhopt3uPaoxJ8jDRTbvce1RiT5GG
+                            im3e49qjEnyMNFNu9x7VGJPkYaKbd7j2qMSfIw0U273HtUUdvHqX60B8n5H8axK9M9FrEr0z01iV
+                            6Z6LWJXpnprEr0z0WsSvTPTWJXpnotYlememsSvTPTWJXpnoskSKqCmhSpUCACP8x//Z`,
+                name: 'test',
+                sha1Hash: 'a658db8045c171c85e42a257654bad0ebfec1ce1',
+                size: 2217,
+                uid: uid
+            };
+           ajModule.createAttachment(dataForCall, data.APIKey, data.URL_AJ).then(function (response) {
+                expect(response.statusCode).to.equal(200);
+                expect(response.statusMessage).to.equal('OK');
+
+                done();
+            });
+        });
+    });
+
+    describe('Call getApplication', function () {
+        it('Should come success response with status code 200, status message OK', function (done) {
+            var dataForCall = {
+                device: {
+                    browser: null,
+                    deviceId: null,
+                    ipAddress: null,
+                    os: {
+                         type: null,
+                         version: null
+                    },
+                    referrer: null,
+                    viewport: {
+                        height: null,
+                        width: null
+                    }
+                },
+                player: {
+                    type: 'web-embedded',
+                    version: '1.20'
+                },
+                uid: uid
+            };
+
+            ajModule.getApplication(dataForCall, data.APIKey, data.URL_AJ).then(function (response) {
+                expect(response.statusCode).to.equal(200);
+                expect(response.statusMessage).to.equal('OK');
 
                 done();
             });
         });
     });
 });
-
